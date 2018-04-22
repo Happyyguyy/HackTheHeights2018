75228d6f9e8541a59a6c74004bab1b6a68ee62e9
ismontiel <31461123+ismontiel@users.noreply.github.com>
2018-04-22 11:00:41 -0400

    Populates the tag panel

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var database = firebase.database().ref();
 
-function newPost() {
+function newPost() {    
+    var newTags = []
     var title = $("#title").val();
     var message = $("#message").val();
     var location = $("#LocTag").val();
@@ -9,7 +10,8 @@ function newPost() {
     
     tagsArr.forEach(function(element) {
         if (!masterTags.includes(element)) {
-            masterTags.push(element)
+            masterTags.push(element);
+            newTags.push(element);
             }
     });
     
@@ -18,6 +20,16 @@ function newPost() {
         'message': message,
         'locTag': location,
         'tagTag': tagsArr
+    });
+    
+    newTags.forEach(function(element) {
+        var div = document.createElement("div");
+        div.innerHTML += '<span style="margin-left: 15px; font-size:30px;cursor:pointer" onclick="openTwo()" >' +
+            '<h2 id="TagCard">' +
+            element +
+            '</h2></span>';
+        $("#tags").append(div);
+        
     });
     
     $("#Post").click(function() {
